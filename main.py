bca7613c0309edfa907de17a314871ca8dc58a40
lfbannon-ux <lfbannon@gmail.com>
2026-02-02 10:37:48 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1093,7 +1093,6 @@ If NO competitive news was found within the 2-month window, return:
                                                                          'april 2025', 'may 2025', 'june 2025',
                                                                          'july 2025', 'august 2025', 'september 2025',
                                                                          'october 2025', 'november 2025']):
-                                                                         'october 2025', 'november 2025']):
                                 continue  # Skip old 2025 dates
                             is_valid = True
                     
